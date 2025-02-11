430056ea40c0a5ef0d72c49c727d94e4e059ac9d
tmdtmddl <tmddlqkqh@naver.com>
2025-02-11 11:02:37 +0900

    develope

--- requirements/src/main.jsx
+++ requirements/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./Home";
+import Login from "./Login";
+import Project from "./Project.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Project />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
